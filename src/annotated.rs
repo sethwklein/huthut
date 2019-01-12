18ba40c365264f6d1f7e11dd449f470ac9a0deab
Seth W. Klein <sk@sethwklein.net>
2019-01-11 22:53:16 -0500

    Rewrite main loop
    I haven’t benchmarked it, but from a clarity perspective, I’m not
    at all sure it’s better. I think what I had in mind when I wanted
    to try is something that can be built with a traditional for loop
    and a function that returns the first UTF-8 encoding from a string
    and its width. In Rust, the traditional for loop is omitted because
    we’re not smart enough to use it, and that function is embedded in
    an iterator. The Rust way may be to do the loop with an iterator.
    I haven’t tried that yet, so I can’t comment on the clarity. Pretty
    sure it’ll be higher ceremony, though.

--- src/annotated.rs
+++ src/annotated.rs
@@ -8,7 +8,7 @@ pub fn annotate<T, U>(items: Vec<T>, make_annotation: fn(&T) -> U) -> Vec<(T, U)
 }
 
 
-pub fn to_parts(string: &str) -> Vec<Part> {
+pub fn to_parts(s: &str) -> Vec<Part> {
     // While it's unlikely the empty input fast path will be triggered here,
     // knowing the input is non-empty simplifies some code below.
     if s.len() < 1 {
@@ -30,37 +30,40 @@ pub fn to_parts(string: &str) -> Vec<Part> {
     // an increment because this doesn't process end of string.
     // Seems like there ought to be a way to get the first UTF-8 character
     // from a string without so many extraneous concepts. --SK
-    let mut prev = !string.chars().next().unwrap().is_whitespace();
-    for c in string.chars() {
+    let mut prev = !s.chars().next().unwrap().is_whitespace();
+    for c in s.chars() {
         if prev != c.is_whitespace() {
             n += 1;
             prev = !prev;
         }
     }
-    // println!("{} {}", n, string);
+    // println!("{} {}", n, s);
 
     let mut parts: Vec<Part> = Vec::with_capacity(n);
-    let mut char_indices = string.char_indices().peekable();
-    let mut boundary = 0;
-
-    while let Some((index, ch)) = char_indices.next() {
-
-        if let Some((_, next_ch)) = char_indices.peek() {
-            if next_ch.is_whitespace() == ch.is_whitespace() {
-                continue;
-            }
+    let mut prev_white = s.chars().next().unwrap().is_whitespace();
+    let mut beg = 0;
+    let mut iter = s.char_indices();
+    loop {
+        // Throwing the push in a function and calling it from both inside
+        // and after the loop would probably be better than ripping a simple
+        // loop up like this. --SK
+        let (i, cur_white) = match iter.next() {
+            Some((i, c)) => (i, c.is_whitespace()),
+            None => (s.len(), !prev_white),
+        };
+        if prev_white != cur_white {
+            let end = i;
+            let sub = &s[beg..end];
+            parts.push(if prev_white {
+                Part::Whitespace(sub)
+            } else {
+                Part::Word(sub)
+            });
+            beg = end;
+            prev_white = !prev_white;
         }
-
-        let prev_boundary = boundary;
-        boundary = index + ch.len_utf8();
-
-        let str_part = &string[prev_boundary..boundary];
-
-        if ch.is_whitespace() {
-            parts.push(Part::Whitespace(str_part));
-        }
-        else {
-            parts.push(Part::Word(str_part));
+        if i >= s.len() {
+            break
         }
     }
 
